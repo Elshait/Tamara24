0a88abca41aac475bfd504598e762831726eaa87
Polya <polinaelsasjt@gmail.com>
2024-04-16 12:51:01 +0700

    доюавили комментарии в исходный код

--- context1.c
+++ context1.c
@@ -1,11 +1,15 @@
+//подключение библеотек
 #include <sys/types.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+//главная функция программы с аргументом 
 int main(int argc, char *argv[], char *envp[]) {
-	int result;
+	int result; 
+	//запуск программы cat с аргументом отчетпт.тчт
 	result = execle("/bin/cat", "/bin/cat", "otchetPT.txt", 0, envp);
+	//принтф выполняется тольо в случае ошибки вызова эксесл
 	printf("Ошибка при выполнении системного execle()\n");
 	exit(-1);
-	return 0;
+	return 0; //никогда не выполняется
 }
