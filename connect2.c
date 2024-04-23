0cd36fa4dda8f48bbb72121f6791a0b87139cfce
Polya <polinaelsasjt@gmail.com>
2024-04-23 15:27:01 +0700

    Добавили комментарии в программу file.txt

--- connect2.c
+++ connect2.c
@@ -3,20 +3,27 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+//Бблиотека управления файлами
 int main  () {
 	int fd;
 	size_t size;
-	char string[17];
+//Инициируем массив символов
+	char string[18];
+//Открфтие файла на чтение
 	if((fd = open("file.txt",O_RDONLY))<0) {
 	printf("ошибка при открытии файла на чтение\n");
 	exit(-1);
 	}
-	size = read(fd, string, 17);
-	if (size !=17){
+//Чтение информаци из фйла
+	size = read(fd, string, 18);
+//Проверка количества прочитанных байт
+	if (size !=18){
 		printf("Не удалось прочетать всю строку файла\n");
 		exit(-1);
 	}
+//Печать строки
 	printf("%s", string);
+//Закрытие файла
 	if(close(fd)<0){
 		printf("Не улдалось закрыть файл\n");
 		exit(-1);
