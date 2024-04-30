7283e5e336a15298152ab889201d6b69b5a42b2c
Polya <polinaelsasjt@gmail.com>
2024-04-30 14:32:22 +0700

    Добавили второй пайп для передачи информации от ребенка к родителю

--- pipe.c
+++ pipe.c
@@ -3,16 +3,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-	int fd[2];
+	int fd1[2], fd2[2];
 	pid_t result;
 	size_t size;
-	char string[] = "Hello, World!";
-	char resstring[14];
+	char string1[] = "Hello, child!", string2[] = "Hello, parent!";
+	char resstring1[13], resstring2[14];
 	//Попытка создать пайп
-	if (pipe(fd)<0) {
+	if (pipe(fd1)<0) {
 		//если пайп не удалось вывести печатаем спредупреждающее сообщение
-		printf("Can\'t create pipe\n");
+		printf("Не получиолось создать pipe()");
 		exit(-1);
+	}
+	if (pipe(fd2)<0) {
+                //если пайп не удалось вывести печатаем спредупреждающее сообщение
+                printf("Не получиолось создать pipe()");
+                exit(-1);
 	}
 	result = fork();
 	if (result<0){
@@ -20,40 +25,70 @@ int main(){
 		exit(-1);
 	}
 	else if (result>0){
-		if (close(fd[0])<0){
+		if (close(fd1[0])<0){
 			printf("Ошибка при закрытии пайп на чтение\n");
 			exit(-1);
 		}
-	
+                if (close(fd2[1])<0){
+                        printf("Ошибка при закрытии пайп на чтение\n");
+                        exit(-1);
+                }
 //Запишем в пайп 14 байт
-		size = write(fd[1], string,14);
-		if(size !=14){
+		size = write(fd1[1], string1,13);
+		if(size !=13){
 			printf("Can\'t write all string\n");
 			exit(-1);
 		}
 		printf("Процесс родитель записал информацию в пайп и заканчивает работу\n");
-		if (close(fd[1])<0){
+		size = read(fd2[0],resstring2,14);
+                if(size != 14){
+                        printf("Can\'t read string\n");
+                        exit(-1);
+		}
+		printf("Информация от дочернего процесса:  %s\n", resstring2);
+
+		if (close(fd1[1])<0){
 			printf("Не получилось закрыть исходящий поток\n");
 			exit(-1);
 		}
+		if (close(fd1[1])<0){
+                        printf("Не получилось закрыть исходящий поток\n");
+                        exit(-1);
+		}
+
 	}
 	else {
-		if (close(fd[1])<0){ 
+		if (close(fd1[1])<0){
                 	printf("Не получилось закрыть исходящий поток\n");
                 	exit(-1);
 		}
+		if (close(fd2[0])<0){ 
+                        printf("Не получилось закрыть входящий поток\n");
+                        exit(-1);
+                }
+
 //Если запись прошла и количество байт меньше, пишем сообщение
-		size = read(fd[0],resstring,14);
-		if(size<0){
+		size = read(fd1[0],resstring1,13);
+		if(size !=13){
 			printf("Can\'t read string\n");
 			exit(-1);
 		}
 //Закрытие пайп для записи
-		printf("%s\n", resstring);
-		if(close(fd[0])<0){
+		printf("%s\n", resstring1);
+		size = write(fd2[1], string2, 14);
+                if(size !=14){
+                        printf("Can\'t write all string\n");
+                        exit(-1);
+                }
+                printf("Процесс ребенок  записал информацию в пайп\n");
+		if(close(fd1[0])<0){
 			printf("Can\'t close output stream\n");
 			exit(-1);
 		}
+		if(close(fd2[1])<0){
+                        printf("Can\'t close output stream\n");
+                        exit(-1);
+               	}
 	}
 	return 0;
 }
