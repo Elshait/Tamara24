823357062446d6bb25cdaaa438f0452b387719b8
Polya <polinaelsasjt@gmail.com>
2024-05-21 13:20:23 +0700

    Добавили в справочную систему описание цикла

--- Help.java
+++ Help.java
@@ -9,12 +9,15 @@ class Help {
 			System.out.println("справочная система:");
 			System.out.println(" 1. if");
 			System.out.println(" 2. switch");
+			System.out.println(" 3. for");
+                        System.out.println(" 4. while");
+			System.out.println(" 5. do-while");
 			System.out.println("Выберите подраздел: ");
 			choice = (char) System.in.read();
 			do {
 				ignore = (char) System.in.read();
 			}while (ignore != '\n');
-		} while (choice < '1' | choice > '2');
+		} while (choice < '1' | choice > '5');
 
 		System.out.println("\n");
 
@@ -32,9 +35,22 @@ class Help {
                                 System.out.println("	break:  ");
                                 System.out.println("	... ");
 				System.out.println(")");
-				break;		
-			default:
-				System.out.print("Неверный номер подраздела");
+				break;	
+  			case '3':
+                                System.out.println("цикл for:\n");
+                                System.out.println("for (инициализация; условие; итерация) ");
+                                System.out.println(" операторы: ");
+                                break;
+			 case '4':
+                                System.out.println("цикл while:\n");
+                                System.out.println("while ( условие) операторы; ");
+                                break;
+			 case '5':
+                                System.out.println("цикл do-while:\n");
+                                System.out.println("do { ");
+                                System.out.println(" операторы: ");
+				System.out.println(" } while (условие); ");
+                                break;
 				
 
 
